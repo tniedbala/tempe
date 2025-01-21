86c3ccf5205ecf7088d9e8f2df025265707ba47e
Tim Niedbala <tniedbala@live.com>
2025-01-21 00:57:23 +0000

    added Env & Template types

---
+++ pkg/env.go
@@ -0,0 +1,68 @@
+package tempe
+
+import (
+	"fmt"
+	"github.com/tniedbala/tempe/pkg/api"
+	"strings"
+)
+
+// Wrapper struct for api.Env, which will be used to implement intended template variable scope.  
+type Env struct {
+	parent *Env
+	env    api.Env
+}
+
+// Create a new Env wrapper around env.
+func NewEnv(env api.Env) *Env {
+	return &Env{env: env}
+}
+
+// Create a new local environment (copy of the parent environment).
+func (e *Env) LocalEnv() (api.Env, error) {
+	localEnv, err := e.env.LocalEnv()
+	if err != nil {
+		return nil, err
+	}
+	if err := localEnv.Update(e.env); err != nil {
+		return nil, err
+	}
+	return &Env{e, localEnv}, nil
+}
+
+func (e *Env) Keys() []string {
+	return e.env.Keys()
+}
+
+func (e *Env) Map() map[string]api.Value {
+	return e.env.Map()
+}
+
+func (e *Env) Get(name string) (api.Value, bool) {
+	return e.env.Get(name)
+}
+
+func (e *Env) Set(name string, value api.Value) error {
+	return e.env.Set(name, value)
+}
+
+// only update variables that exist in the current env
+func (e *Env) Update(env api.Env) error {
+	for _, name := range e.Keys() {
+		value, ok := env.Get(name)
+		if !ok {
+			continue
+		}
+		if err := e.Set(name, value); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *Env) Eval(src string) (api.Value, error) {
+	return e.env.Eval(src)
+}
+
+func (e Env) String() string {
+	return fmt.Sprintf("Env{%s}", strings.Join(e.Keys(), ", "))
+}
---
+++ pkg/template.go
@@ -0,0 +1,102 @@
+package tempe
+
+import (
+	"fmt"
+	"bytes"
+	"github.com/tniedbala/tempe/pkg/api"
+	"github.com/tniedbala/tempe/pkg/parser"
+	"io"
+	"os"
+)
+
+type Template struct {
+	engine api.TemplateEngine
+	env    *Env
+	parser *parser.TemplateParser
+	tree   *parser.ParseTree
+}
+
+// TODO: this is incorrect, need to review how to read from reader of unknown size
+func Read(engine api.TemplateEngine, reader io.Reader) (*Template, error) {
+	var b []byte
+	if _, err := reader.Read(b); err != nil {
+		return nil, err
+	}
+	fmt.Println(b)
+	return NewTemplate(engine, string(b))
+}
+
+func ReadFile(engine api.TemplateEngine, path string) (*Template, error) {
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return NewTemplate(engine, string(b))
+}
+
+func NewTemplate(engine api.TemplateEngine, src string) (*Template, error) {
+	template := &Template{
+		engine: engine,
+		env:    NewEnv(engine.DefaultEnv()),
+		parser: parser.NewTemplateParser(),
+	}
+	err := template.Parse(src)
+	return template, err
+}
+
+func (t *Template) Engine() api.TemplateEngine {
+	return t.engine
+}
+
+func (t *Template) Env() api.Env {
+	return t.env
+}
+
+func (t *Template) SetEnv(env map[string]any) error {
+	e, err := t.engine.NewEnv(env)
+	if err != nil {
+		return err
+	}
+	t.env = NewEnv(e)
+	return nil
+}
+
+func (t *Template) Parse(src string) error {
+	tree, err := t.parser.Parse(src)
+	if err != nil {
+		return err
+	}
+	t.tree = tree
+	return nil
+}
+
+func (t *Template) ParseTree() api.ParseTree {
+	return t.tree
+}
+
+func (t *Template) Render(params map[string]any) (string, error) {
+	var b bytes.Buffer
+	if err := t.Write(params, &b); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
+
+func (t *Template) Write(params map[string]any, w io.StringWriter) error {
+	localEnv, err := t.env.LocalEnv()
+	if err != nil {
+		return err
+	}
+	for key, val := range params {
+		value, err := t.engine.NewValue(val)
+		if err != nil {
+			return err
+		}
+		localEnv.Set(key, value)
+	}
+	return t.tree.Node().Render(localEnv, w)
+}
+
+func (t Template) String() string {
+	return "Template{}"
+}
