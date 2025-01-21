38c7da0885e42155b61d1394280e037c57ca8fcd
Tim Niedbala <tniedbala@live.com>
2025-01-21 00:56:41 +0000

    added ParseTree & ParseTreeFormatter

---
+++ pkg/parser/parse_tree.go
@@ -0,0 +1,53 @@
+package parser
+
+import (
+	"github.com/tniedbala/tempe/pkg/api"
+	"iter"
+)
+
+type ParseTree struct {
+	parent *ParseTree
+	node   api.TemplateNode
+	depth  int
+}
+
+func NewParseTree(node api.TemplateNode, parent *ParseTree) *ParseTree {
+	depth := 0
+	if parent != nil {
+		depth = parent.depth + 1
+	}
+	return &ParseTree{parent, node, depth}
+}
+
+func (p *ParseTree) Depth() int {
+	return p.depth
+}
+
+func (p *ParseTree) Length() int {
+	return len(p.node.Children())
+}
+
+func (p *ParseTree) Parent() api.ParseTree {
+	return p.parent
+}
+
+func (p *ParseTree) Node() api.TemplateNode {
+	return p.node
+}
+
+func (p *ParseTree) Children() iter.Seq2[int, api.ParseTree] {
+	return func(yield func(int, api.ParseTree) bool) {
+		children := p.node.Children()
+		for i, node := range children {
+			tree := NewParseTree(node, p)
+			if !yield(i, tree) {
+				return
+			}
+		}
+	}
+}
+
+func (p *ParseTree) PrettyPrint() (string, error) {
+	formatter := NewParseTreeFormatter(nil, p, false)
+	return formatter.PrettyPrint()
+}
---
+++ pkg/parser/parse_tree_formatter.go
@@ -0,0 +1,84 @@
+package parser
+
+import (
+	"bytes"
+	"io"
+	"github.com/tniedbala/tempe/pkg/api"
+)
+
+type ParseTreeFormatter struct {
+	tree      api.ParseTree
+	parent    *ParseTreeFormatter
+	node      api.TemplateNode
+	depth     int
+	lastChild bool
+	indent    string
+}
+
+func NewParseTreeFormatter(parent *ParseTreeFormatter, tree api.ParseTree, lastChild bool) *ParseTreeFormatter {
+	formatter := &ParseTreeFormatter{
+		parent:    parent,
+		tree:      tree,
+		node:      tree.Node(),
+		depth:     tree.Depth(),
+		lastChild: lastChild,
+	}
+	return formatter
+}
+
+func (p *ParseTreeFormatter) Branch() string {
+	if p.lastChild {
+		return "└── "
+	} else {
+		return "├── "
+	}
+}
+
+func (p *ParseTreeFormatter) ParentIndent() string {
+	if p.depth <= 1 {
+		return ""
+	}
+	if p.parent == nil {
+		return ""
+	}
+	return p.parent.indent
+}
+
+func (p *ParseTreeFormatter) Indent() string {
+	if p.depth <= 1 {
+		return ""
+	}
+	if p.parent != nil && p.parent.lastChild {
+		return "    "
+	}
+	return "│   "
+}
+
+func (p *ParseTreeFormatter) Render(w io.StringWriter) error {
+	var newline, branch, str string
+	if p.depth == 0 {
+		newline, branch, str = "", "", "Template:"
+	} else {
+		newline, branch, str = "\n", p.Branch(), p.node.String()
+	}
+	p.indent = p.ParentIndent() + p.Indent()
+	if _, err := w.WriteString(newline + p.indent + branch + str); err != nil {
+		return err
+	}
+	lastIndex := p.tree.Length() - 1
+	for i, tree := range p.tree.Children() {
+		formatter := NewParseTreeFormatter(p, tree, i==lastIndex)
+		if err := formatter.Render(w); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (p *ParseTreeFormatter) PrettyPrint() (string, error) {
+	var b bytes.Buffer
+	if err := p.Render(&b); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
